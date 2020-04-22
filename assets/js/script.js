2361fe3dc706cb7eb68e2ffd21506cb28ba73579
Sarah Choung <sarahjchoung@gmail.com>
2020-04-21 17:14:47 -0700

    MM flip cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+var main = document.getElementById("gameCards");
+main.addEventListener("click", handleClick);
 
+function handleClick(event) {
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  console.log(event);
+  var target = event.target;
+  target.classList.add("hidden")
+}
