aee5da13ec3df14db527749bd57f88b7a3785d6f
Sarah Choung <sarahjchoung@gmail.com>
2020-04-21 20:42:51 -0700

    Memory Match - Math Two Cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+var firstCardClicked;
+var secondCardClicked;
+var firstCardClasses;
+var secondCardClasses;
+
+
 var main = document.getElementById("gameCards");
 main.addEventListener("click", handleClick);
 
@@ -5,7 +11,35 @@ function handleClick(event) {
   if (event.target.className.indexOf("card-back") === -1) {
     return;
   }
-  console.log(event);
   var target = event.target;
   target.classList.add("hidden")
+
+//Match 2 Cards
+  if (!firstCardClicked) {
+    firstCardClicked = target;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  } else {
+    secondCardClicked = target;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    main.removeEventListener("click", handleClick);
+
+    if (firstCardClasses === secondCardClasses) {
+      main.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+    } else {
+      setTimeout(function() {
+        firstCardClicked.classList.remove("hidden");
+        secondCardClicked.classList.remove("hidden");
+        main.addEventListener("click", handleClick);
+        firstCardClicked = null;
+        secondCardClicked = null;
+
+      }, 1500);
+
+    }
+  }
+
+
+
 }
